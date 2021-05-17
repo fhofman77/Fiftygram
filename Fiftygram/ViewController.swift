44903b9c8094e65de268ae8e9842bdcbf6d74783
Frank Hofman <frank-hofman77@live.nl>
2021-05-17 14:04:01 +0200

    added own filter buttons, still trying to create a customer filter

--- Fiftygram/ViewController.swift
+++ Fiftygram/ViewController.swift
@@ -7,14 +7,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     var original: UIImage!
     
     @IBAction func savePhoto() {
-        UIImageWriteToSavedPhotosAlbum(original, nil, nil, nil)
+        if original == nil {
+            displayNoImage()
+            return
+        }
+        
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, nil, nil, nil)
+        let alert = UIAlertController(title: "Done!", message: "Your image is saved.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Oké", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction func randomFilter() {
         if original == nil {
-            let alert = UIAlertController(title: "No image", message: "Choose an image", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Will do!", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            displayNoImage()
             return
         }
         let randomizer = Int.random(in: 1...4)
@@ -47,6 +53,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBAction func frankfilter() {
         if original == nil {
+            displayNoImage()
             return
         }
         
@@ -57,6 +64,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func applySepia() {
         if original == nil {
+            displayNoImage()
             return
         }
 
@@ -68,6 +76,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func applyNoir() {
         if original == nil {
+            displayNoImage()
             return
         }
 
@@ -78,6 +87,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBAction func applyVintage() {
         if original == nil {
+            displayNoImage()
             return
         }
 
@@ -109,5 +119,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imageView.image = image
             original = image
         }
+    }
+    
+    func displayNoImage() {
+        let alert = UIAlertController(title: "No image", message: "Choose an image!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Will do!", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
